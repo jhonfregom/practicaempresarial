4f5223e5ce0780b981e76162c7da9a76d78d8140
grupogoogle <jhonfregom@gmail.com>
2018-04-10 22:44:25 -0500

    act

--- phpform-master/includes/connection.php
+++ phpform-master/includes/connection.php
@@ -6,10 +6,10 @@ require("constants.php");
 	mysqli_select_db(DB_NAME) or die("No se logra conexion");*/
 
 	function connectDB(){
-		$server = "172.30.13.23";
+		$server = "172.30.223.141";
 		$user = "user";
 		$pass = "password";
-		$bd = "practica";
+		$bd = "sampledb";
 		
 		$con = mysqli_connect($server, $user, $pass,$bd) 
 			or die("Ha sucedido un error inexperado en la conexion de la base de datos");
